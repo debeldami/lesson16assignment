5dfda2e0560a643466ba5800711291345256de70
Coltrane Yan <coltraneyan@gmail.com>
2022-07-17 20:45:02 -0400

    simplified routes and db

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -39,113 +7,18 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const node_json_db_1 = require("node-json-db");
 const JsonDBConfig_1 = require("node-json-db/dist/lib/JsonDBConfig");
-const ipfs_http_client_1 = require("ipfs-http-client");
-const utils_1 = require("./utils");
-const multer_1 = __importDefault(require("multer"));
-const fs_1 = __importStar(require("fs"));
-const path = require('path');
 dotenv_1.default.config();
-const ipfsClient = (0, ipfs_http_client_1.create)({
-    host: 'localhost',
-    port: 5001,
-    protocol: 'http',
-});
-const upload = (0, multer_1.default)({ dest: 'uploads/' });
-const DB_PATH = path.join('db', 'MetaData');
-let db = new node_json_db_1.JsonDB(new JsonDBConfig_1.Config(DB_PATH, true, true, '/'));
+let db = new node_json_db_1.JsonDB(new JsonDBConfig_1.Config("db/MetaData", true, true, "/"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const port = process.env.PORT;
-const data = db.getData('/');
-let lastId = data && Object.keys(data).length > 0
-    ? Math.max(...Object.keys(data).map((key) => Number(key)))
-    : -1;
-app.get('/', (req, res) => {
-    let data = db.getData('/');
-    res.send(data);
+app.get("/", (req, res) => {
+    let data = db.getData(`/`);
+    res.status(200).send(data);
 });
-app.get('/ipfs', (0, utils_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.json({
-        status: yield ipfsClient.isOnline(),
-    });
-})));
-app.get('/file/:id', (req, res) => {
-    const fileId = req.params.id;
-    try {
-        const fileData = db.getData(`/${fileId}`).file;
-        const readStream = (0, fs_1.createReadStream)(`./uploads/${fileData.filename}`);
-        readStream.on('open', function () {
-            readStream.pipe(res);
-        });
-        res.set({
-            'Content-Type': fileData.mimetype,
-            'Content-Disposition': `attachment; filename="${fileData.filename}"`,
-        });
-    }
-    catch (error) {
-        res.json({
-            error,
-        });
-    }
-});
-app.get('/:id', (req, res) => {
-    res.json(db.getData(`/${req.params.id}`));
-});
-app.get('/ipfs-get/:id', (0, utils_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () { })));
-app.post('/file', upload.single('nft'), (req, res) => {
-    if (req.file) {
-        ++lastId;
-        const { originalname, mimetype, filename, size } = req.file;
-        db.push(`/${lastId}`, {
-            file: {
-                originalname,
-                mimetype,
-                filename,
-                size,
-            },
-        });
-        res.json(Object.assign({}, req.file));
-    }
-    else {
-        res.status(400).json({
-            status: 'failed',
-        });
-    }
-});
-app.post('/ipfs-save', (0, utils_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { fileId } = req.body;
-        const fileData = db.getData(`/${fileId}`).file;
-        const fileBytes = fs_1.default.readFileSync(`./uploads/${fileData.filename}`);
-        const ipfsData = yield ipfsClient.add(fileBytes);
-        db.push(`/${fileId}/ipfs`, ipfsData);
-        res.json(ipfsData);
-    }
-    catch (error) {
-        res.json({
-            status: 'error occur',
-        });
-    }
-})));
-app.post('/metadata', (req, res) => {
-    const { fileId, metadata } = req.body;
-    console.log(fileId, metadata);
-    const { oS, hoodieColor, skinTone, color } = metadata;
-    let file;
-    try {
-        file = db.getData(`/${fileId}/file`);
-    }
-    catch (error) {
-        return res.json({
-            error: 'an error occur',
-        });
-    }
-    if (!file)
-        return res.json({
-            error: 'an error occur',
-        });
-    db.push(`/${fileId}/metadata`, { oS, hoodieColor, skinTone, color });
-    return res.json(db.getData(`/${fileId}`));
+app.get("/:id", (req, res) => {
+    let data = db.getData(`/${req.params.id}`);
+    res.status(200).send(data);
 });
 app.listen(port, () => {
     console.log(`Server is running at https://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,27 +1,11 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { JsonDB } from 'node-json-db';
-import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
-import { create } from 'ipfs-http-client';
-import { asyncHandler } from './utils';
-import multer from 'multer';
-
-import fs, { createReadStream } from 'fs';
-const path = require('path');
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { JsonDB } from "node-json-db";
+import { Config } from "node-json-db/dist/lib/JsonDBConfig";
 
 dotenv.config();
 
-const ipfsClient = create({
-  host: 'localhost',
-  port: 5001,
-  protocol: 'http',
-});
-
-const upload = multer({ dest: 'uploads/' });
-
-const DB_PATH = path.join('db', 'MetaData');
-
-let db = new JsonDB(new Config(DB_PATH, true, true, '/'));
+let db = new JsonDB(new Config("db/MetaData", true, true, "/"));
 
 const app: Express = express();
 
@@ -29,131 +13,14 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
-const data = db.getData('/');
-let lastId =
-  data && Object.keys(data).length > 0
-    ? Math.max(...Object.keys(data).map((key) => Number(key)))
-    : -1;
-
-app.get('/', (req: Request, res: Response) => {
-  let data = db.getData('/');
-  res.send(data);
+app.get("/", (req: Request, res: Response) => {
+  let data = db.getData(`/`);
+  res.status(200).send(data);
 });
 
-app.get(
-  '/ipfs',
-  asyncHandler(async (req: Request, res: Response) => {
-    res.json({
-      status: await ipfsClient.isOnline(),
-    });
-  })
-);
-
-app.get('/file/:id', (req: Request, res: Response) => {
-  const fileId = req.params.id;
-  try {
-    const fileData = db.getData(`/${fileId}`).file;
-
-    const readStream = createReadStream(`./uploads/${fileData.filename}`);
-
-    readStream.on('open', function () {
-      readStream.pipe(res);
-    });
-
-    res.set({
-      'Content-Type': fileData.mimetype,
-      'Content-Disposition': `attachment; filename="${fileData.filename}"`,
-    });
-  } catch (error) {
-    res.json({
-      error,
-    });
-  }
-});
-
-app.get('/:id', (req: Request, res: Response) => {
-  res.json(db.getData(`/${req.params.id}`));
-});
-
-app.get(
-  '/ipfs-get/:id',
-  asyncHandler(async (req: Request, res: Response) => {
-    //TODO
-  })
-);
-
-app.post('/file', upload.single('nft'), (req: Request, res: Response) => {
-  if (req.file) {
-    ++lastId;
-
-    const { originalname, mimetype, filename, size } = req.file;
-
-    db.push(`/${lastId}`, {
-      file: {
-        originalname,
-        mimetype,
-        filename,
-        size,
-      },
-    });
-
-    res.json({
-      ...req.file,
-    });
-  } else {
-    res.status(400).json({
-      status: 'failed',
-    });
-  }
-});
-
-app.post(
-  '/ipfs-save',
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const { fileId } = req.body;
-
-      const fileData = db.getData(`/${fileId}`).file;
-
-      const fileBytes = fs.readFileSync(`./uploads/${fileData.filename}`);
-
-      const ipfsData = await ipfsClient.add(fileBytes);
-
-      db.push(`/${fileId}/ipfs`, ipfsData);
-
-      res.json(ipfsData);
-    } catch (error) {
-      res.json({
-        status: 'error occur',
-      });
-    }
-  })
-);
-
-app.post('/metadata', (req: Request, res: Response) => {
-  const { fileId, metadata } = req.body;
-  console.log(fileId, metadata);
-
-  const { oS, hoodieColor, skinTone, color } = metadata;
-
-  let file;
-
-  try {
-    file = db.getData(`/${fileId}/file`);
-  } catch (error) {
-    return res.json({
-      error: 'an error occur',
-    });
-  }
-
-  if (!file)
-    return res.json({
-      error: 'an error occur',
-    });
-
-  db.push(`/${fileId}/metadata`, { oS, hoodieColor, skinTone, color });
-
-  return res.json(db.getData(`/${fileId}`));
+app.get("/:id", (req: Request, res: Response) => {
+  let data = db.getData(`/${req.params.id}`);
+  res.status(200).send(data);
 });
 
 app.listen(port, () => {
